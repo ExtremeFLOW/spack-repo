9c832aae72058242930c400bb2474c8c04202688
Niclas Jansson <njansson@kth.se>
2022-02-05 16:45:18 +0100

    Backport fixes

--- packages/neko/package.py
+++ packages/neko/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -6,26 +6,20 @@
 from spack import *
 
 
-class Neko(AutotoolsPackage,CudaPackage,ROCmPackage):
-    """N E K O"""
+class Neko(AutotoolsPackage, CudaPackage, ROCmPackage):
+    """Neko: A modern, portable, and scalable framework
+       for high-fidelity computational fluid dynamics
+    """
 
     homepage = "https://github.com/ExtremeFLOW/neko"
     git      = "https://github.com/ExtremeFLOW/neko.git"
-
     maintainers = ['njansson']
 
     version('0.3.0', tag='v0.3.0')
-    version('0.2.2', tag='v0.2.2')
-    version('0.2.1', tag='v0.2.1')
-    version('0.2.0', tag='v0.2.0')
-    version('0.1.3', tag='v0.1.3')
-    version('0.1.2', tag='v0.1.2')
-    version('0.1.1', tag='v0.1.1')
-    version('0.1.0', tag='v0.1.0')
     version('develop', branch='develop')
     variant('parmetis', default=False, description='Build with support for parmetis')
     variant('xsmm', default=False, description='Build with support for libxsmm')
-    
+
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool',  type='build')
@@ -36,9 +30,6 @@ class Neko(AutotoolsPackage,CudaPackage,ROCmPackage):
     depends_on('mpi')
     depends_on('blas')
     depends_on('lapack')
-
-    conflicts('+xsmm', when='+cuda')
-    conflicts('+rocm', when='+cuda')
 
     def configure_args(self):
         args = []
